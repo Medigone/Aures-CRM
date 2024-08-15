1a2a1e2b4c68977ef151af116631598f3d0e6f54
Medigone <admin@medigo.one>
2024-08-15 16:29:20 +0200

    New: Fixtures in Hooks & Export

--- aurescrm/hooks.py
+++ aurescrm/hooks.py
@@ -228,3 +228,10 @@ app_license = "mit"
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
 
+fixtures = [
+    "Custom HTML Block",
+    "Communes",
+    "Wilayas",
+    "Territory",
+    "Document Naming Rule"
+]
