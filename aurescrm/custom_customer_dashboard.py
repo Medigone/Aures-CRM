97849aaba4409e401f07ff0e2ad45c313df2a7e0
Medigone <admin@medigo.one>
2024-08-29 16:01:44 +0200

    Update: removing opportunite

--- aurescrm/custom_customer_dashboard.py
+++ aurescrm/custom_customer_dashboard.py
@@ -4,11 +4,10 @@ def get_data(**kwargs):
     return {
         "fieldname": "customer",
         "non_standard_fieldnames": {
-            "Opportunite": "client",
             "Visite Commerciale": "client",  
             "Appel Telephonique": "client",  
         },
         "transactions": [
-            {"items": ["Opportunite", "Visite Commerciale", "Appel Telephonique"]},
+            {"items": ["Visite Commerciale", "Appel Telephonique"]},
         ],
     }
