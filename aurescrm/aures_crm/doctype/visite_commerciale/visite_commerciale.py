93e638cda6aaa6399eb63f109fafa6ac51d61af8
Medigone <admin@medigo.one>
2024-09-25 22:02:11 +0200

    New: Server scripts Heure et temps

--- aurescrm/aures_crm/doctype/visite_commerciale/visite_commerciale.py
+++ aurescrm/aures_crm/doctype/visite_commerciale/visite_commerciale.py
@@ -1,15 +1,30 @@
 # Copyright (c) 2024, Medigo and contributors
 # For license information, please see license.txt
 
-# import frappe
 from frappe.model.document import Document
-
-
-class VisiteCommerciale(Document):
-	pass
-
 import frappe
 from frappe.model.mapper import get_mapped_doc
+from frappe.utils import now_datetime  # Import pour gérer la date et l'heure
+
+class VisiteCommerciale(Document):
+    def before_save(self):
+        # Vérifie si le statut est "Terminé" et que 'heure_fin_visite' n'est pas encore défini
+        if self.status == "Terminé" and not self.heure_fin_visite:
+            now = now_datetime()
+
+            # Met à jour le champ 'heure_fin_visite' avec l'heure actuelle
+            self.heure_fin_visite = now
+
+            # Si les champs 'heure_debut_visite' et 'heure_fin_visite' sont tous deux présents, calculer la durée
+            if self.heure_debut_visite and self.heure_fin_visite:
+                start_time = frappe.utils.get_datetime(self.heure_debut_visite)
+                end_time = frappe.utils.get_datetime(self.heure_fin_visite)
+
+                # Calcul de la durée en minutes
+                duration_in_minutes = (end_time - start_time).total_seconds() / 60
+
+                # Arrondir la durée à l'entier le plus proche
+                self.duree_visite = round(duration_in_minutes)
 
 @frappe.whitelist()
 def make_quotation_from_visite_commerciale(source_name, target_doc=None):
