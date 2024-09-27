ec12326433c911c6be77b73544328367e7cc89ca
Medigone <admin@medigo.one>
2024-09-27 11:23:41 +0200

    Update: user & user name

--- aurescrm/aures_crm/doctype/tirages_blanc/tirages_blanc.py
+++ aurescrm/aures_crm/doctype/tirages_blanc/tirages_blanc.py
@@ -3,7 +3,15 @@
 
 # import frappe
 from frappe.model.document import Document
-
+import frappe
 
 class TiragesBlanc(Document):
-	pass
+    def before_save(self):
+        # 1. Vérifier si le champ 'utilisateur' est vide, et le remplir avec l'ID (email) du créateur (owner)
+        if not self.utilisateur:
+            self.utilisateur = self.owner  # Définit 'utilisateur' avec le propriétaire initial (créateur)
+
+        # 2. Remplir le champ 'nom_utilisateur' avec le nom complet de l'utilisateur
+        if self.utilisateur:
+            # Récupérer le nom complet de l'utilisateur via frappe.get_fullname
+            self.nom_utilisateur = frappe.utils.get_fullname(self.utilisateur)
