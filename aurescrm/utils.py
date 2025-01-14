e8a7243b367db2a4ea2204d6db5ff5ff848aadc1
Medigone <admin@medigo.one>
2025-01-14 16:42:40 +0100

    Update: item naming by customer

--- aurescrm/utils.py
+++ aurescrm/utils.py
@@ -4,11 +4,18 @@ import frappe
 def custom_item_naming(doc, method):
     if doc.item_group == "Produits":  # Vérifie si le groupe d'article est 'Produits'
         if doc.custom_client:  # Vérifie si un client est lié
-            customer_id = frappe.get_value("Customer", doc.custom_client, "name")  # Récupère l'ID du client
+            # Récupère l'ID du client
+            customer_id = frappe.get_value("Customer", doc.custom_client, "name")
             if not customer_id:
                 frappe.throw("Le champ Client est invalide ou vide.")
-            # Générer le code au format CLI-0001-0001
-            doc.name = make_autoname(f"{customer_id}-.####")
+            
+            # Compte le nombre d'articles existants pour ce client
+            existing_count = frappe.db.count("Item", filters={"custom_client": doc.custom_client})
+            
+            # Génère le compteur pour ce client
+            next_number = f"{existing_count + 1:03}"  # Format 001, 002, etc.
+            
+            # Génère le nom complet
+            doc.name = f"{customer_id}-{next_number}"
         else:
             frappe.throw("Veuillez sélectionner un client pour générer un code Item.")
-   
