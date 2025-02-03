61b243d45006247146007f57928fd3c7bcbf9745
Medigone <admin@medigo.one>
2025-02-03 17:52:23 +0100

    Ajout d'un tableau de bord HTML pour afficher les informations des clients associés au groupe de sociétés. Suppression du champ 'status' et ajout de nouveaux champs 'image' et 'html' dans le doctype.

--- aurescrm/aures_crm/doctype/groupe_de_societes/groupe_de_societes.js
+++ aurescrm/aures_crm/doctype/groupe_de_societes/groupe_de_societes.js
@@ -6,3 +6,84 @@
 
 // 	},
 // });
+frappe.ui.form.on('Groupe de societes', {
+    refresh: function(frm) {
+        if (!frm.doc.__islocal) {  // Vérifie si le document est enregistré
+            frappe.call({
+                method: 'frappe.client.get_list',
+                args: {
+                    doctype: 'Customer',
+                    filters: {
+                        'custom_groupe_de_sociétés': frm.doc.name
+                    },
+                    fields: ['name', 'customer_name']
+                },
+                callback: function(response) {
+                    if (response.message) {
+                        let customers = response.message;
+                        let customerData = [];
+
+                        let promises = customers.map(customer => {
+                            return new Promise(resolve => {
+                                frappe.call({
+                                    method: "frappe.client.get_list",
+                                    args: {
+                                        doctype: "Sales Invoice",
+                                        filters: {
+                                            "customer": customer.name,
+                                            "docstatus": 1  // Factures soumises
+                                        },
+                                        fields: ["grand_total", "outstanding_amount"]
+                                    },
+                                    callback: function(salesResponse) {
+                                        let total_billed = 0;
+                                        let outstanding_amount = 0;
+
+                                        if (salesResponse.message) {
+                                            salesResponse.message.forEach(invoice => {
+                                                total_billed += invoice.grand_total;
+                                                outstanding_amount += invoice.outstanding_amount;
+                                            });
+                                        }
+
+                                        customerData.push({
+                                            customer_name: customer.customer_name,
+                                            total_billed: total_billed,
+                                            outstanding_amount: outstanding_amount
+                                        });
+
+                                        resolve();
+                                    }
+                                });
+                            });
+                        });
+
+                        Promise.all(promises).then(() => {
+                            let html = `<table class="table table-bordered">
+                                            <thead>
+                                                <tr>
+                                                    <th>Nom du Client</th>
+                                                    <th>Chiffre d'Affaires</th>
+                                                    <th>Solde Actuel</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>`;
+
+                            customerData.forEach(customer => {
+                                html += `<tr>
+                                            <td>${customer.customer_name}</td>
+                                            <td>${format_currency(customer.total_billed, frappe.defaults.get_default("currency"))}</td>
+                                            <td>${format_currency(customer.outstanding_amount, frappe.defaults.get_default("currency"))}</td>
+                                         </tr>`;
+                            });
+
+                            html += `</tbody></table>`;
+
+                            frm.set_df_property('html', 'options', html);
+                        });
+                    }
+                }
+            });
+        }
+    }
+});
