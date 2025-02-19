e86d7db5d5c257ad0e70c4e91d8a0ccc192faf74
Medigone <admin@medigo.one>
2025-02-19 16:46:00 +0100

    Désactivation du chargement du fichier de fixtures 'Role' dans le module Aures CRM et export des docperm

--- aurescrm/hooks.py
+++ aurescrm/hooks.py
@@ -265,7 +265,7 @@ scheduler_events = {
 
 fixtures = [
     # "Custom HTML Block",
-    "Role",
+    # "Role",
     # "Client Script",
     # "Document Naming Rule",
     # "Workflow",
@@ -274,4 +274,5 @@ fixtures = [
     # "Workflow Action",
     # "Workflow Action Master",
     # "Workflow Document State",
+    # "DocPerm"
 ]
