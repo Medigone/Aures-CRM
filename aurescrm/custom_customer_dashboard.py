135557fa56cd6cb097404de0028ff7f84c35ef75
Medigone <admin@medigo.one>
2025-02-26 12:07:49 +0100

    Ajout de 'Sales Invoice' aux données du tableau de bord client et réorganisation des éléments dans les transactions.

--- aurescrm/custom_customer_dashboard.py
+++ aurescrm/custom_customer_dashboard.py
@@ -14,6 +14,7 @@ def get_data(**kwargs):
             "Etude Technique": "client",
             "Reclamations Clients": "client",
             "Etude Faisabilite": "client",
+            "Sales Invoice": "customer",
         },
         "transactions": [
             {
@@ -22,11 +23,11 @@ def get_data(**kwargs):
             },
             {
                
-                "items": ["Item", "Etude Faisabilite", "Quotation", "Sales Order"],
+                "items": ["Item", "Quotation", "Sales Order", "Sales Invoice"],
             },
             {
                
-                "items": ["Etude Technique", "BOM"],
+                "items": ["Etude Technique", "BOM", "Etude Faisabilite"],
             },
             
             
