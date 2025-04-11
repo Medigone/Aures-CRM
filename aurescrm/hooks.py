b8e60daca0ef21e6dcc62a4cf85579dc5b568763
Medigone <admin@medigo.one>
2025-04-11 15:26:17 +0200

    Correction des fautes d'orthographe dans les rôles "Responsable Étude Faisabilité" dans plusieurs fichiers JSON et ajout de nouveaux états et actions de workflow.

--- aurescrm/hooks.py
+++ aurescrm/hooks.py
@@ -281,25 +281,24 @@ scheduler_events = {
 # }
 
 fixtures = [
-    #     {
-    #     "doctype": "Role",
-    #     "filters": [["is_custom", "=", 1]]  # Export uniquement les rôles custom
-    # },
-    # {
-    #     "doctype": "Custom DocPerm",
-    #     "filters": []  # Export uniquement les permissions personnalisées
-    # },
+        {
+        "doctype": "Role",
+        "filters": [["is_custom", "=", 1]]  # Export uniquement les rôles custom
+    },
+    {
+        "doctype": "Custom DocPerm",
+        "filters": []  # Export uniquement les permissions personnalisées
+    },
     # "Custom HTML Block",
     # "Client Script",
     # "Document Naming Rule",
-    # "Workflow",
-    # "Workflow State",
-    # "Workflow Transition",
-    # "Workflow Action",
-    # "Workflow Action Master",
-    # "Workflow Document State",
+    "Workflow",
+    "Workflow State",
+    "Workflow Transition",
+    "Workflow Action",
+    "Workflow Action Master",
+    "Workflow Document State",
     # "Workspace",
-    # "Custom DocPerm",
     # "Number Card"
     
     
