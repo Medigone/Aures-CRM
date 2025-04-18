c474d521bb4487b060d997c178ceb9d1dd77ed3d
Medigone <admin@medigo.one>
2025-04-18 18:35:53 +0200

    feat(maquette): add status management and referencing functionality
    Introduce new status management for maquettes, including the ability to mark a maquette as "Référencée" with user attribution. Add validation logic to clear the referencing user when the status is not "Référencée". Update the UI to include a custom button for marking a maquette as referenced and enhance the form with additional fields and filters. Modify the JSON schema to support these changes, including new fields for referencing and versioning.

--- aurescrm/aures_crm/doctype/maquette/maquette.js
+++ aurescrm/aures_crm/doctype/maquette/maquette.js
@@ -1,8 +1,33 @@
-// Copyright (c) 2025, Medigo and contributors
-// For license information, please see license.txt
+frappe.ui.form.on('Maquette', {
+    onload: function(frm) {
+        if (frm.is_new() || frm.doc.status !== 'Référencée') {
+            frm.set_value('nom_reference_par', '');
+        }
+    },
 
-// frappe.ui.form.on("Maquette", {
-// 	refresh(frm) {
+    refresh: function(frm) {
+        if (!frm.is_new() && frm.doc.status === "En attente") {
+            frm.add_custom_button("Référencée", function() {
+                frappe.confirm(
+                    'Confirmez-vous que le fichier source de la maquette a été renommé avec cet ID dans le serveur des maquettes ?',
+                    function() {
+                        frm.call('set_status_referenced').then(() => {
+                            frappe.msgprint("La maquette a été marquée comme référencée dans le serveur");
+                            frm.reload_doc();
+                        });
+                    }
+                );
+            });
+        }
+    },
 
-// 	},
-// });
+    setup: function(frm) {
+        frm.set_query('article', function() {
+            return {
+                filters: {
+                    'custom_client': frm.doc.client
+                }
+            };
+        });
+    }
+});
--- aurescrm/aures_crm/doctype/maquette/maquette.py
+++ aurescrm/aures_crm/doctype/maquette/maquette.py
@@ -1,9 +1,19 @@
 # Copyright (c) 2025, Medigo and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class Maquette(Document):
-	pass
+    def validate(self):
+        if self.is_new() or self.status != 'Référencée':
+            self.nom_reference_par = ''
+
+    @frappe.whitelist()
+    def set_status_referenced(self):
+        if self.status == "En attente":
+            self.status = "Référencée"
+            self.nom_reference_par = frappe.session.user
+            self.save()
+            return True
+        return False
