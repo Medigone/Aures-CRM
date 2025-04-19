534f08c08adedecafc5c58cd82356511219ed0ca
Medigone <admin@medigo.one>
2025-04-19 16:42:02 +0200

    feat(bat): add autoname method to generate BAT document name
    Implement the autoname method to automatically generate a unique name for BAT documents in the format "BAT-{client}-{article}-{etude_tech}". This improves document identification and reduces manual naming errors.

--- aurescrm/aures_crm/doctype/bat/bat.py
+++ aurescrm/aures_crm/doctype/bat/bat.py
@@ -1,9 +1,14 @@
 # Copyright (c) 2025, Medigo and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class BAT(Document):
+	def autoname(self):
+		# Format: BAT-{client}-{article}-{etude_tech}
+		self.name = f"BAT-{self.client}-{self.article}-{self.etude_tech}"
+
+	# You can add other methods like validate, before_save, etc. here if needed
 	pass
