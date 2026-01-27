a72ae2d4b97e660dbfadb9ccc2133170c125ebd1
Medigone <admin@medigo.one>
2026-01-27 13:36:57 +0100

    update(annuaire_interne): modify field structure and timestamps
    - Adjusted the field order in 'annuaire_interne.json' to include 'photo' and 'column_break_swqf' for better organization.
    - Updated the 'creation' and 'modified' timestamps for consistency.
    - Ensured 'departement' is now positioned correctly in the field order.

---
+++ aurescrm/aures_crm/doctype/annuaire_interne/annuaire_interne.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Medigo and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Annuaire Interne", {
+// 	refresh(frm) {
+
+// 	},
+// });
